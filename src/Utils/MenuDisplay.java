e4c0c8f45012c0dd65b94cce6c5240c3703cc665
Arya Raihan Hanif <aryaraihanhanif@gmail>
2025-06-06 14:17:21 +0700

    nambahin apa gitu

--- src/Utils/MenuDisplay.java
+++ src/Utils/MenuDisplay.java
@@ -4,6 +4,8 @@ public class MenuDisplay {
     public static void showMainMenu() {
         System.out.println("\n===== PS5 Preorder System =====");
         System.out.println("1. Add New Preorder");
+        System.out.println("1. Something New");
+        System.out.println("1. Sesuatu yang baru");
         System.out.println("2. Process Next Preorder");
         System.out.println("3. View Current Front Preorder");
         System.out.println("4. Exit");
