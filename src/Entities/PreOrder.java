59845235224def6cd0438206a7a425bbdafef4c0
Biagiarchiefz <biagiiarchie@gmail.com>
2025-06-06 20:46:51 +0700

    test

--- src/Entities/PreOrder.java
+++ src/Entities/PreOrder.java
@@ -1,26 +1,5 @@
 package Entities;
 
 public class PreOrder {
-    private String customerName;
-    private String contactNumber;
 
-    // Constructor
-    public PreOrder(String name, String contact) {
-        this.customerName = name;
-        this.contactNumber = contact;
-    }
-
-    // Getters
-    public String getCustomerName() { return customerName; }
-    public String getContactNumber() { return contactNumber; }
-
-
-    // Setters
-    public void setCustomerName(String name) { this.customerName = name; }
-    public void setContactNumber(String contact) { this.contactNumber = contact; }
-
-    @Override
-    public String toString() {
-        return "Preorder{" + "customerName='" + customerName + '\'' + ", contactNumber='" + contactNumber + '\'' + '}';
-    }
 }
