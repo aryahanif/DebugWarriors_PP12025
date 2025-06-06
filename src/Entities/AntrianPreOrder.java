39e45d43ab2696278aa84250203b6797cbe4c5a3
Biagiarchiefz <biagiiarchie@gmail.com>
2025-06-06 23:50:47 +0700

    menambahkan entity AntrianPreOrder dan PS5Model

---
+++ src/Entities/AntrianPreOrder.java
@@ -0,0 +1,58 @@
+package Entities;
+
+public class AntrianPreOrder {
+    private Customer customer;
+    private PS5Model model;
+    private String nomorAntrian;
+    private String waktuMasukAntrian;
+    private String status;
+
+    // Constructor
+    public AntrianPreOrder(Customer customer, PS5Model model, String nomorAntrian, String waktuMasukAntrian) {
+        this.customer = customer;
+        this.model = model;
+        this.nomorAntrian = nomorAntrian;
+        this.waktuMasukAntrian = waktuMasukAntrian;
+        this.status = "DALAM ANTRIAN"; // Status default
+    }
+
+    // Getters
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public PS5Model getModel() {
+        return model;
+    }
+
+    public String getNomorAntrian() {
+        return nomorAntrian;
+    }
+
+    public String getWaktuMasukAntrian() {
+        return waktuMasukAntrian;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    // Setter
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
+    @Override
+    public String toString() {
+        return "==============================" +
+                "\nNomor Antrian   : " + nomorAntrian +
+                "\nNama Pelanggan  : " + customer.getNamaCust() +
+                "\nKontak          : " + customer.getNoKontak() +
+                "\nModel PS5       : " + model.getNamaModel() +
+                "\nHarga           : Rp" + String.format("%,.0f", model.getHarga()) +
+                "\nWaktu Daftar    : " + waktuMasukAntrian +
+                "\nStatus          : " + status +
+                "\n==============================";
+    }
+}
---
+++ src/PreOrderApp.java
@@ -0,0 +1,9 @@
+
+public class PreOrderApp {
+    public static void main(String[] args) {
+
+    }
+
+    }
+
+
